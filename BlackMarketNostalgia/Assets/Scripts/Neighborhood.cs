299a9c1321ef9f9c5b6acbecd33e81d9b7ec0e4b
Ash Fischer <ash.s.fischer@gmail.com>
2018-08-11 12:37:51 -0500

    ADD tooltips and stats to neighborhoods

--- BlackMarketNostalgia/Assets/Scripts/Neighborhood.cs
+++ BlackMarketNostalgia/Assets/Scripts/Neighborhood.cs
@@ -1,4 +1,6 @@
-﻿public enum Temperament
+﻿using UnityEngine;
+
+public enum Temperament
 {
     Outraged, Apathetic, Neutral, Sypathetic, Patriotic,
 }
@@ -8,27 +10,55 @@ public enum Income
     Poverty, LowerMiddle, Middle, UpperMiddle, Upper, Ruling,
 }
 
-public class Neighborhood
+public class Neighborhood : MonoBehaviour
 {
 
-    private string m_name;
-    private int m_population;
-    private int m_corporateInfluence;
-    private Temperament m_temperament;
-    private Income m_income;
+    [SerializeField] string m_name;
+    [SerializeField] int m_population;
+    [Range(0,100)]
+    [SerializeField] int m_corporateInfluence;
+    [SerializeField] Temperament m_temperament;
+    [SerializeField] Income m_income;
 
-    public void NeighborhoodSetUp(string name, int population, int corporateInfluence, Temperament temperament, Income income)
-    {
-        m_name = name;
-        m_population = population;
-        m_corporateInfluence = corporateInfluence;
-        m_temperament = temperament;
-        m_income = income;
-    } 
+    private GUIStyle guiStyleBox;
+    private string text;
+    private string currentToolTipText = "";
 
-    public int CorporateInfluence
+    private void Start()
     {
-        get { return m_corporateInfluence; }
-        set { m_corporateInfluence -= value; }
+        // TODO use to customize GUI later
+        guiStyleBox = new GUIStyle();
+        FormatText();
     }
+
+    private void FormatText()
+    {
+        text = "\nName: " + m_name + "\n"
+            + "Population: " + m_population + "\n"
+            + "Corporate Influence: " + m_corporateInfluence.ToString() + "\n"
+            + "Temperament: " + m_temperament.ToString() + "\n"
+            + "Income: " + m_income.ToString();
+    }
+
+    private void OnGUI()
+    {
+        if (currentToolTipText != "")
+        {
+            // follow mouse pos
+            float x = Event.current.mousePosition.x;
+            float y = Event.current.mousePosition.y;
+            GUI.Box(new Rect(x + 10, y + 10, 300, 110), currentToolTipText);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        currentToolTipText = text;
+    }
+
+    private void OnMouseExit()
+    {
+        currentToolTipText = "";
+    }
+
 }
--- BlackMarketNostalgia/Assets/Scripts/Player.cs
+++ BlackMarketNostalgia/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
     // TODO create a list to store items
     List<Upgrade> upgrades = new List<Upgrade>();
